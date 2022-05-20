b709df72d67376ed18f162ed9cb6478dd406b79c
usama shmasneh <us.05.07.91@gmail.com>
2022-05-20 17:08:07 +0300

    Add project files.

---
+++ Recipes/Controllers/RecipeModelsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Data;
+using Recipes.Models;
+
+namespace Recipes.Controllers
+{
+    public class RecipeModelsController : Controller
+    {
+        private readonly RecipesContext _context;
+
+        public RecipeModelsController(RecipesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RecipeModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.RecipeModel.ToListAsync());
+        }
+
+        // GET: RecipeModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeModel = await _context.RecipeModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeModel);
+        }
+
+        // GET: RecipeModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecipeModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Recipe")] RecipeModel recipeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recipeModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recipeModel);
+        }
+
+        // GET: RecipeModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeModel = await _context.RecipeModel.FindAsync(id);
+            if (recipeModel == null)
+            {
+                return NotFound();
+            }
+            return View(recipeModel);
+        }
+
+        // POST: RecipeModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Recipe")] RecipeModel recipeModel)
+        {
+            if (id != recipeModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipeModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeModelExists(recipeModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recipeModel);
+        }
+
+        // GET: RecipeModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeModel = await _context.RecipeModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeModel);
+        }
+
+        // POST: RecipeModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recipeModel = await _context.RecipeModel.FindAsync(id);
+            _context.RecipeModel.Remove(recipeModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeModelExists(int id)
+        {
+            return _context.RecipeModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Recipes/Models/RecipeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Recipes.Models
+{
+    public class RecipeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Recipe { get; set; }
+    }
+}
